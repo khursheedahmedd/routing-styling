f527301b373f5d82b41d3b99f66e448367bccbf7
khursheedahmedd <118465990+khursheedahmedd@users.noreply.github.com>
2024-08-12 22:17:45 +0500

    ..

---
+++ src/app/components/display-name/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const DisplayName = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/");
+  };
+
+  return (
+    <>
+      <div className="text-center mt-8">
+        My name is Khursheed Ahmed. But people call me pro-programmer...
+      </div>
+      <div className="flex justify-center py-8">
+        <button
+          onClick={handleClick}
+          className="border border-gray-400 rounded-lg p-2 bg-gradient-to-r from-purple-300 to-pink-500 opacity-65 hover:opacity-100 "
+        >
+          Click here to go back
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default DisplayName;
